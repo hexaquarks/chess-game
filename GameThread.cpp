9f7fffc366cd4089573d20df7cff153c17c7fb4a
hexaquarks <seseriusca@gmail.com>
2022-01-07 12:50:28 -0500

    Begun mouse events handling for a piece that is being dragged

--- GameThread.cpp
+++ GameThread.cpp
@@ -16,30 +16,65 @@ void GameThread::startGame() {
         true);  // prevent tearing, forces app to run at same FPS as monitor.
     Event event;
 
+    // parameters to handle a piece being dragged
+    bool pieceIsMoving;
+    Piece* selectedPiece;
+    int xPos;
+    int yPos;
+
     // this is the main loop (a.k.a game loop) this ensures that the program
     // does not terminate until we exit.
     while (window.isOpen()) {
+        window.clear(sf::Color::Black);
         //  we use a while loop for the pending events in case there were
         //  multiple events occured
-        while (window.pollEvent(event))
-            if (event.type == Event::Closed) { 
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
                 window.close();
-            } else if (event.type == Event::MouseButtonPressed) {
-                if(event.mouseButton.button == Mouse::Left) {
+            }
 
+            if (event.type == sf::Event::MouseButtonPressed) {
+
+                if (event.mouseButton.button == sf::Mouse::Left) {
                     // get the tile of the click
-                    int xPos = event.mouseButton.x / 80; 
-                    int yPos = event.mouseButton.y / 80; 
+                    xPos = event.mouseButton.x;
+                    yPos = event.mouseButton.y;
 
-                    if(game->getBoardTile(i,j) != nullptr) {
-                        
+                    if (game->getBoardTile(xPos / 80, yPos / 80) != nullptr) {
+                        std::cout << "Succesfully clicked a piece" << std::endl;
+                        selectedPiece = game->getBoardTile(xPos, yPos);
+                        pieceIsMoving = true;
+
+                        // set the tile on the board where the piece is selected, to
+                        // null and draw the dragged piece explicitly
+                        game->_board[xPos / 80][yPos / 80] = nullptr;
                     }
+
                 }
             }
 
+            if (event.type == sf::Event::MouseMoved && pieceIsMoving) {
+                // update the position of the piece that is being moved
+                xPos = event.mouseButton.x;
+                yPos = event.mouseButton.y;
+            }
+            if (event.type == sf::Event::MouseButtonReleased) {
+                if (event.mouseButton.button == sf::Mouse::Left) {
+                    if (game->getBoardTile(xPos / 80, yPos / 80) == nullptr) {
+                        // drop a piece to the tile only if is empty (for
+                        // testing)
+                        game->_board[xPos / 80][yPos / 80] = selectedPiece;
+                    }
+
+                    xPos = -1;
+                    yPos = -1;
+                    selectedPiece = nullptr;
+                    pieceIsMoving = false;
+                }
+            }
+        }
         // initializing the board
-        
-        window.clear(sf::Color::Black);
+
         for (int i = 0; i < 8; ++i) {
             for (int j = 0; j < 8; ++j) {
                 // drawing the colored square
@@ -55,21 +90,28 @@ void GameThread::startGame() {
                 window.draw(square);
 
                 // drawing the Piece if there is one
-
                 if (game->_board[i][j] != nullptr) {
-                    std::cout << "the current (row,col) is " << i << "," << j
-                              << std::endl;
                     Texture t;
                     t.loadFromFile(game->getBoardTile(i, j)->_filename);
-                    std::cout << "file name is "
-                              << game->getBoardTile(i, j)->_filename
-                              << std::endl;
                     Sprite tt(t);
 
                     tt.setScale(0.6, 0.6);
                     tt.setPosition(i * 80, j * 80);
 
                     window.draw(tt);
+                }
+
+                // draw the piece that is being dragged
+                if (pieceIsMoving) {
+                    Texture t;
+                    t.loadFromFile(selectedPiece->_filename);
+                    Sprite tt(t);
+                    tt.setScale(0.6, 0.6);
+                    tt.setPosition(xPos, yPos);
+                    window.draw(tt);
+
+                    std::cout << "Succesfully drawed the new piece"
+                              << std::endl;
                 }
             }
         }
