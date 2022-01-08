e8eee8cb10b7b9eb3c12d3f9a22dd39783fd1036
hexaquarks <seseriusca@gmail.com>
2022-01-08 14:17:02 -0500

    Capture/one tile move/2 tile move cases completed

--- Pawn.cpp
+++ Pawn.cpp
@@ -5,28 +5,49 @@
 Pawn::Pawn(Team team, int xPos, int yPos)
     : Piece(team, xPos, yPos, PieceType::PAWN, "p") {}
 
-void Pawn::calcPossibleMoves(Piece* board[8][8]) const {
+vector<tuple<pair<int, int> , MoveType>> Pawn::calcPossibleMoves(Piece* board[8][8]) const {
     vector<tuple<pair<int, int> , MoveType>> moves;
     int x = Piece::xPos;
     int y = Piece::yPos;
 
-    // can only move one square forward
-    if (y + 1 == 7) {
-        // pawn is queening
+    int dir = Pawn::getTeam() == Team::WHITE ? 1 : -1;
 
-    } else if (y + 1 > 1 && y + 1 <= 6) {
-        // normal movement
-        if(board[x][y+1] == nullptr) 
-                moves.push_back(make_tuple(make_pair(xPos, yPos+1), MoveType::NORMAL));
+    /* Capture
+     * Need to do each side explicitly
+     * 1. taking rightwards
+     */ 
+    if(x + 1 <= 7 && (y + dir <=7 && y + dir >= 0)) {
+        if(board[x+1][y+dir]->getTeam() != Pawn::getTeam()) {
+            moves.push_back(make_tuple(make_pair(xPos+1, yPos+dir), MoveType::CAPTURE));
+        }        
+    } 
 
-        if(!Pawn::hasMoved){
-            if(board[x][y+2] == nullptr) 
-                moves.push_back(make_tuple(make_pair(xPos, yPos+2), MoveType::NORMAL));
-        } 
+    // 2. taking leftwards
+    if (x - 1 >=0 && (y + dir <=7 && y + dir >= 0)) {
+        if(board[x-1][y+dir]->getTeam() != Pawn::getTeam()) {
+            moves.push_back(make_tuple(make_pair(xPos-1, yPos+dir), MoveType::CAPTURE));
+        }        
     }
-    
-    // en passant
-    
 
-    // capture
-}
+    // normal one tile move 
+    if(y + dir == 0 || y + dir == 7) {
+        // pawn hitting the edge of the board (queeing)
+        if(board[x][y+dir] == nullptr) 
+                moves.push_back(make_tuple(make_pair(xPos, yPos+dir), MoveType::NEWPIECE));
+
+    } else {
+        // pawn hitting a square on the board
+        if(board[x][y+dir] == nullptr) 
+                moves.push_back(make_tuple(make_pair(xPos, yPos+dir), MoveType::NORMAL));
+    }
+
+    // double square initial move
+    if((y == 1 || y == 6) && !Pawn::hasMoved) {
+        if(board[x][y+2*dir] == nullptr) 
+                moves.push_back(make_tuple(make_pair(xPos, yPos+2*dir), MoveType::NORMAL));
+    }
+
+    // En passant &TODO
+
+    return moves;
+};
