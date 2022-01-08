77b9cc9cf3021d763cea2c2dc2e549e2e1b1e0f0
hexaquarks <seseriusca@gmail.com>
2022-01-08 17:52:54 -0500

    Fixed typo and removed local variable for position

--- Knight.cpp
+++ Knight.cpp
@@ -1,5 +1,26 @@
 #include "Knight.h"
+#include <vector>
 
 Knight::Knight(Team team, int x, int y): Piece(team, x, y, PieceType::KNIGHT, "n") {}
 
-vector<tuple<pair<int, int> , MoveType>> Knight::calcPossibleMoves(Piece* board[8][8]) const {}
+vector<tuple<pair<int, int> , MoveType>> Knight::calcPossibleMoves(Piece* board[8][8]) const {
+    vector<tuple<pair<int, int> , MoveType>> moves;
+
+    // the possible moves
+    int X[8] = {2,1,-1,-2,-2,-1,1,2};
+    int Y[8] = {1,2,2,1,-1,-2,-2,-1};
+
+    for(int i = 0; i < 8; ++i) {
+        int x = Knight::xPos + X[i];
+        int y = Knight::yPos + Y[i];
+
+        if(x >= 0 && y >= 0 && x < 8 && y < 8) {
+            if(board[xs][ys] == nullptr) {
+                moves.push_back(make_tuple(make_pair(xs,ys), MoveType::NORMAL));
+            } else {
+                if(board[xs][ys]->getTeam() != Knight::getTeam())
+                    moves.push_back(make_tuple(make_pair(xs,ys), MoveType::CAPTURE));
+            }
+        }
+    }
+}
