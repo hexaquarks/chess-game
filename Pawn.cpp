e4692824d44f5580f54fa92e677638ff821df1d0
hexaquarks <seseriusca@gmail.com>
2022-01-08 11:20:23 -0500

    Started pawn possible moves functions

--- Pawn.cpp
+++ Pawn.cpp
@@ -1,5 +1,37 @@
 #include "Pawn.h"
 
-Pawn::Pawn(Team team, int xPos, int yPos): Piece(team, xPos, yPos, PieceType::PAWN, "p") {}
+#include <vector>
 
-void Pawn::calcPossibleMoves(Piece* board[8][8]) const {}
+Pawn::Pawn(Team team, int xPos, int yPos)
+    : Piece(team, xPos, yPos, PieceType::PAWN, "p") {}
+
+void Pawn::calcPossibleMoves(Piece* board[8][8]) const {
+    vector<tuple<pair<int, int> , MoveType>> moves;
+    int x = Piece::xPos;
+    int y = Piece::yPos;
+
+    // move forward
+
+    // can only move one square forward
+    if (y + 1 == 7) {
+        // pawn is queening
+    } else if (y + 1 > 1 && y + 1 <= 6) {
+        // normal movement
+        
+        if(Pawn::hasMoved){
+            if(board[x][y+1] == nullptr) 
+                moves.push_back(make_tuple(make_pair(xPos, yPos+1), MoveType::NORMAL));
+        } else {
+            if(board[x][y+1] == nullptr) 
+                moves.push_back(make_tuple(make_pair(xPos, yPos+1), MoveType::NORMAL));
+            if(board[x][y+2] == nullptr) 
+                moves.push_back(make_tuple(make_pair(xPos, yPos+2), MoveType::NORMAL));
+        }
+        
+    }
+    if (y + 1 <= 7) {
+    }
+    // en passant
+
+    // capture
+}
