c3a12f4942c478a517c42ae4a19ab84468f8e892
hexaquarks <seseriusca@gmail.com>
2022-01-08 14:15:25 -0500

    Changed return type of calcMoves()

--- Pawn.h
+++ Pawn.h
@@ -1,10 +1,11 @@
 #pragma once
 #include "Piece.h"
+#include <vector>
 
 
 // Represents a pawns
 struct Pawn : public Piece {
     Pawn(Team, int, int); // Constructor
-    void calcPossibleMoves(Piece*[8][8]) const override;
+    vector<tuple<pair<int, int> , MoveType>> calcPossibleMoves(Piece*[8][8]) const override;
     bool hasMoved;
 };
