ee2a05f6d604a175d49664b6f6086c1cb9af2fdc
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2022-01-13 00:24:48 +0100

    King castling - part 2

--- King.h
+++ King.h
@@ -6,6 +6,13 @@
 class King : public Piece {
     moveTypes possibleMovesNoCheck(Piece*[8][8]) const;
     bool isChecked(Piece*[8][8], int, int) const;
+    bool canCastleKingSide(Piece*[8][8]) const;
+    bool canCastleQueenSide(Piece*[8][8]) const;
+    static void swapPieces(Piece* board[8][8], int x, int y, int X, int Y) {
+        Piece* first = board[x][y];
+        board[x][y] = board[X][Y];
+        board[X][Y] = first;
+    }
 
     public:
     King(Team, int, int); // Constructor
