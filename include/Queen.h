5216025664975f8cf198c2fc839fdcc2df88aa58
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2022-01-14 15:11:58 +0100

    Delete Queen.h

--- include/Queen.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-
-
-// Represents a queen
-struct Queen : public Piece {
-    Queen(Team, int, int); // Constructor
-    moveTypes calcPossibleMoves(Piece*[8][8]) const override;
-};
