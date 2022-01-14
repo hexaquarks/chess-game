3d6e214bfc4dcee6d06004ac1f2d4e89defbb425
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2022-01-14 15:11:40 +0100

    Delete Knight.h

--- include/Knight.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-
-
-// Represents a knight
-struct Knight : public Piece {
-    Knight(Team, int, int); // Constructor
-    moveTypes calcPossibleMoves(Piece*[8][8]) const override;
-};
