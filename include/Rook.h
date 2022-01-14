1b8a62c41fc2da004a2fb06ce4163dd8e73d159f
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2022-01-14 15:12:04 +0100

    Delete Rook.h

--- include/Rook.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-
-
-// Represents a rook
-struct Rook : public Piece {
-    Rook(Team, int, int); // Constructor
-    moveTypes calcPossibleMoves(Piece*[8][8]) const override;
-};
