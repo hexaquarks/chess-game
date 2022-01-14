5db6d5ec20cbafe374152adc322b8bbc7dc6d329
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2022-01-14 15:11:12 +0100

    Delete Bishop.h

--- include/Bishop.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Piece.h"
-
-
-// Represents a bishop
-struct Bishop : public Piece {
-    Bishop(Team, int, int); // Constructor
-    moveTypes calcPossibleMoves(Piece*[8][8]) const override; 
-};
