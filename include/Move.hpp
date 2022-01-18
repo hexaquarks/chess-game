729d1f297e251012ce3b7a3adb1ba569a14de26b
hexaquarks <seseriusca@gmail.com>
2022-01-17 21:28:31 -0500

    Added 2 more constructors to differentiate move type

--- include/Move.hpp
+++ include/Move.hpp
@@ -1,13 +1,21 @@
 #include "Piece.hpp"
 
+typedef tuple<pair<int, int>, pair<int, int>> moveFullCoords;
+
 class Move {
 
-    Move* m_lastMove;
-    Piece* m_selectedPiece;
-    Piece* m_takenOrMovedPiece;
-    moveType m_moveType; 
+    int m_xTarget; int m_yTarget; // the destination square of the piece that is being moved
+    moveFullCoords m_secondPieceCoords; // the last move 
+    Piece* m_selectedPiece; // the piece that is being selected
+    Piece* m_takenOrMovedPiece; // the captured piece, the moved rook in castling, or taken pawn in en passant
+    moveType* m_moveType; 
 
     public:
-    Move(Piece*, Piece*, moveType, Move*); // Constructor
+    Move(int,int, Piece*, moveType*); // Constructor for NORMAL and INIT SPECIAL
+    Move(int,int, Piece*, Piece*, moveType*); // Constructor for CAPTURE
+    Move(int,int, Piece*, Piece*, moveType*, moveFullCoords); // Constructor EN PASSANT and CASTLE
+    
+
+    moveFullCoords getMoveCoords() const;
 
 };
--- src/Move.cpp
+++ src/Move.cpp
@@ -1,10 +1,25 @@
 #include "../include/Move.hpp"
 
 
-Move::Move(int x, int y, Piece* selectedPiece, Piece* secondPiece, moveType moveType, Move* lastMove)
+// for NORMAL and INIT SPECIAL 
+Move::Move(int x, int y, Piece* selectedPiece, moveType* moveType)
     : m_xTarget(x),
       m_yTarget(y),
-      m_selectedPiece(secondPiece),
+      m_selectedPiece(selectedPiece),
+      m_moveType(moveType) {};
+
+// for CAPTURE, ENPASSANT, CASTLE
+Move::Move(int x, int y, Piece* selectedPiece, Piece* secondPiece, moveType* moveType, moveFullCoords secondPieceCoords)
+    : m_xTarget(x),
+      m_yTarget(y),
+      m_selectedPiece(selectedPiece),
       m_takenOrMovedPiece(secondPiece),
       m_moveType(moveType),
-      m_lastMove(lastMove) {};
+      m_secondPieceCoords(secondPieceCoords) {};
+
+moveFullCoords Move::getMoveCoords() const {
+     return make_tuple(
+         make_pair( m_selectedPiece->getX(), m_selectedPiece->getY()),
+         make_pair( m_xTarget, m_yTarget)
+     );
+};
