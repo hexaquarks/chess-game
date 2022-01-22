6c6b42ccc1cef909d3d920c0d0a9488748b8e973
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2022-01-22 17:22:05 +0100

    Added nullptr for safety

--- src/Move.cpp
+++ src/Move.cpp
@@ -1,29 +1,29 @@
 #include "../include/Move.hpp"
 
 
-// for NORMAL, INIT SPECIAL and CASTLE
-Move::Move(int x, int y, int xs, int ys, Piece* selectedPiece, MoveType moveType)
-    : m_xTarget(x),
-      m_yTarget(y),
-      m_xInit(xs),
-      m_yInit(ys),
-      m_selectedPiece(selectedPiece),
-      m_MoveType(moveType) { };
+// For NORMAL, INIT SPECIAL and CASTLE
+Move::Move(int x, int y, int xs, int ys, Piece* selectedPiece, MoveType moveType):
+  m_xTarget(x),
+  m_yTarget(y),
+  m_xInit(xs),
+  m_yInit(ys),
+  m_selectedPiece(selectedPiece),
+  m_capturedPiece(nullptr),
+  m_MoveType(moveType) {}
 
-// for CAPTURE and ENPASSANT
-Move::Move(int x, int y,int xs, int ys, Piece* selectedPiece, Piece* secondPiece, MoveType moveType)
-    : m_xTarget(x),
-      m_yTarget(y),
-      m_xInit(xs),
-      m_yInit(ys),
-      m_selectedPiece(selectedPiece),
-      m_capturedPiece(secondPiece),
-      m_MoveType(moveType) { };
-
+// For CAPTURE and ENPASSANT
+Move::Move(int x, int y,int xs, int ys, Piece* selectedPiece, Piece* secondPiece, MoveType moveType):
+  m_xTarget(x),
+  m_yTarget(y),
+  m_xInit(xs),
+  m_yInit(ys),
+  m_selectedPiece(selectedPiece),
+  m_capturedPiece(secondPiece),
+  m_MoveType(moveType) {}
 
 moveFullCoords Move::getMoveCoords() const {
-     return make_tuple(
-         make_pair( m_selectedPiece->getX(), m_selectedPiece->getY()),
-         make_pair( m_xTarget, m_yTarget)
-     );
-};
+  return make_tuple(
+    make_pair(m_selectedPiece->getX(), m_selectedPiece->getY()),
+    make_pair(m_xTarget, m_yTarget)
+  );
+}
