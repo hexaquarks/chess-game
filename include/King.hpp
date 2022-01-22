2539a905e6c1522743877e06433df4439b598562
hexaquarks <seseriusca@gmail.com>
2022-01-21 19:07:15 -0500

    Made swapPieces() public to be accessible in Board class

--- include/King.hpp
+++ include/King.hpp
@@ -8,14 +8,15 @@ class King : public Piece {
     bool isChecked(Piece*[8][8], int, int) const;
     bool canCastleKingSide(Piece*[8][8]) const;
     bool canCastleQueenSide(Piece*[8][8]) const;
-    static void swapPieces(Piece* board[8][8], int x, int y, int X, int Y) {
-        Piece* first = board[x][y];
-        board[x][y] = board[X][Y];
-        board[X][Y] = first;
-    }
 
     public:
     King(Team, int, int); // Constructor
     moveTypes calcPossibleMoves(Piece*[8][8]) const override;
     bool isChecked(Piece* board[8][8]) const { return isChecked(board, getX(), getY()); }
+    
+    static void swapPieces(Piece* board[8][8], int x, int y, int X, int Y) {
+        Piece* first = board[x][y];
+        board[x][y] = board[X][Y];
+        board[X][Y] = first;
+    }
 };
