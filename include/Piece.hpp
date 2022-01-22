718a2c16a9a4893f1fd962c7c2afaab5796c1b2d
hexaquarks <seseriusca@gmail.com>
2022-01-21 19:20:15 -0500

    Begun reorganization og .hpp files

--- include/Piece.hpp
+++ include/Piece.hpp
@@ -16,6 +16,7 @@ typedef vector<moveType> moveTypes;
 
 
 class Piece {
+    /* members */
     inline static Piece* m_lastPiece = nullptr; // Last moved piece
     string m_filename; // the filename for this piece
     Team m_team; // the team this piece plays for
@@ -24,26 +25,29 @@ class Piece {
     int m_xPos; int m_yPos; // X and Y positions
     bool m_moved = false; // Whether piece has moved or not
 
-    public:
+public:
+    /* constructors */
     Piece(Team, int, int, PieceType, string); // Constructor
     virtual ~Piece() {} // Virtual destructor
 
+    /* getters */
     Team getTeam() const { return m_team; };
     PieceType getType() const { return m_type; }
     string getFileName() const { return m_filename; }
     MoveType getLastMove() const { return m_lastMove; }
-    void setLastMove(MoveType newMove) { m_lastMove = newMove; }
-
     static Piece* getLastMovedPiece() { return m_lastPiece; }
-    static void setLastMovedPiece(Piece* piece) { m_lastPiece = piece; }
-
-    bool hasMoved() const { return m_moved; }
     int getX() const { return m_xPos; }
     int getY() const { return m_yPos; }
-
-    virtual moveTypes calcPossibleMoves(Piece*[8][8]) const = 0; // Pure virtual function
+    bool hasMoved() const { return m_moved; }
     moveTypes getHorizontalAndVerticalMovements(Piece*[8][8]) const;
     moveTypes getDiagonalMovements(Piece*[8][8]) const;
+
+    /* setters */
+    void setLastMove(MoveType newMove) { m_lastMove = newMove; }
+    static void setLastMovedPiece(Piece* piece) { m_lastPiece = piece; }
+
+    /* utility functions */
+    virtual moveTypes calcPossibleMoves(Piece*[8][8]) const = 0; // Pure virtual function
     void move(int x, int y, bool record = true) {
         if (record && (m_xPos != x || m_yPos != y)) m_moved = true;
         m_xPos = x;
