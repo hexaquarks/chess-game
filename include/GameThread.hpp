55d3b38b3967d5d5e5b9f57f7d04fbbbc13a832a
TheOneMusic <44474247+TheOneMusic@users.noreply.github.com>
2022-01-27 13:46:35 +0100

    Added constants + made menu bar bigger

--- include/GameThread.hpp
+++ include/GameThread.hpp
@@ -7,9 +7,11 @@ using namespace sf;
 const string WINDOW_TITLE = "Chess Game";
 constexpr uint32_t WINDOW_SIZE = 640;
 constexpr uint32_t CELL_SIZE = WINDOW_SIZE / 8;
-constexpr uint32_t MENUBAR_HEIGHT = 25;
+constexpr uint32_t MENUBAR_HEIGHT = 60;
+constexpr uint32_t NUMBER_BUTTONS = 6;
 constexpr float SPRITE_SCALE = 0.6;
 constexpr float SPRITE_SIZE = 128;
+constexpr float BUTTON_SIZE = 40;
 
 class GameThread {
     inline const static string iconsPath = "../assets/icons/";
