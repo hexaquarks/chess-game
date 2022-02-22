9a325787009d8304401d9c564b72ecee01261ca0
hexaquarks <seseriusca@gmail.com>
2022-02-22 14:53:56 -0500

    Fixed piece arrival bug. up,down,left,right buttons now work perfectly

--- src/PieceTransitioning.cpp
+++ src/PieceTransitioning.cpp
@@ -6,27 +6,29 @@ void PieceTransition::setIncrement() {
     if(m_piece == nullptr) return; 
 
     // piece should arrive at destination in one second
-    m_increment.first = (m_destination.first - m_currPos.first) / (FPS / 4);
-    m_increment.second = (m_destination.second - m_currPos.second) / (FPS / 4);
+    m_increment.first = (m_destination.first - m_currPos.first) / (FPS / 6);
+    m_increment.second = (m_destination.second - m_currPos.second) / (FPS / 6);
+
+    // divide by 6 so the  increment is in base 10 , that is 60 / 10
+    // avoiding any piece to jump a square in a game tick
 }
 
 bool PieceTransition::pieceIsInBounds() {
     if(m_piece == nullptr ) return false;
 
     // stop if arrived in the tile for temporary logical simplicity
-    if (m_currPos.first * CELL_SIZE == m_destination.first * CELL_SIZE)
-        m_xArrived = true;
-    if (m_currPos.second * CELL_SIZE == m_destination.second * CELL_SIZE)
-        m_yArrived = true;
+    if (m_currPos.first == m_destination.first )  m_xArrived = true;
+    if (m_currPos.second == m_destination.second ) m_yArrived = true;
 
     return m_xArrived && m_yArrived;
 }
 
 void PieceTransition::move() { 
     if(m_piece == nullptr) return; 
+    cout << m_destination.first << "," << m_destination.second << endl;
     // move the piece only if piece is not in destinantion bounds yet.
-    m_currPos.first = m_xArrived ? m_currPos.first : m_currPos.first + m_increment.first;
-    m_currPos.second = m_yArrived ? m_currPos.second : m_currPos.second + m_increment.second;
+    if(!m_xArrived) m_currPos.first += m_increment.first;
+    if(!m_yArrived) m_currPos.second += m_increment.second;
 }
 
 void PieceTransition::setHasArrived(bool b, Board& board) {
