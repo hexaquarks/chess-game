f3518599450b3280014438a4b74dcd93673f2f82
Nicolas Almerge <nicolas.almerge@gmail.com>
2022-02-28 23:27:45 -0500

    Made RessourceManager class static

--- include/RessourceManager.hpp
+++ include/RessourceManager.hpp
@@ -4,7 +4,7 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <memory>
-
+#include "Piece.hpp"
 using namespace std;
 
 typedef unordered_map<string, shared_ptr<sf::Texture>> textureMap;
@@ -14,26 +14,28 @@ class RessourceManager {
     inline const static string iconsPath = "../assets/icons/";
     inline const static string audioPath = "../assets/sounds/";
     inline static constexpr uint8_t m_numberOfTextures = 24;
-    string m_textureNames[m_numberOfTextures] = {"circle.png", "empty_circle.png", 
+    inline const static string m_textureNames[m_numberOfTextures] = {"circle.png", "empty_circle.png", 
         "bb.png", "bw.png", "kb.png", "kw.png", "nb.png", "nw.png", "pb.png", "pw.png",
         "qb.png", "qw.png", "rb.png", "reset.png", "rw.png", "dropDown.png", "flip.png",
         "arrow_n1x.png", "arrow_n2x.png", "arrow_n3x.png", "arrow_n4x.png", "arrow_n5x.png",
         "arrow_n6x.png", "arrow_n7x.png"};
 
     // audioVector m_audios;
-    textureMap m_textures;
+    inline static textureMap m_textures;
 
     public:
-    RessourceManager(); // Constructor
-    // ~RessourceManager(); // destructor
+    RessourceManager() = delete;
 
-    sf::SoundBuffer getSound();  
-    void setSounds();
+    static sf::SoundBuffer getSound();  
+    static void setSounds();
 
-    void loadRessources();
+    static void loadRessources();
 
-    void addTexture(const string& name);
-    shared_ptr<sf::Texture> getTexture(const string &name);
+    static void addTexture(const string& name);
+    static shared_ptr<sf::Texture> getTexture(const string &name);
+    static shared_ptr<sf::Texture> getTexture(const Piece* piece) {
+        return getTexture(piece->getFileName());
+    }
 
     static string getIconPath(const string& filename) { return iconsPath + filename; }
     static string getAudioPath(const string& filename) { return audioPath + filename; }
