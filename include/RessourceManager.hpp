1f547145a572d9b20393c398e7778b46a57d63fe
hexaquarks <seseriusca@gmail.com>
2022-03-03 19:13:15 -0500

    Added diagonal arrows as well

--- include/RessourceManager.hpp
+++ include/RessourceManager.hpp
@@ -16,12 +16,15 @@ class RessourceManager {
     inline const static string audioPath = "../assets/sounds/";
     inline const static string fontPath = "../assets/fonts/";
 
-    inline static constexpr uint8_t m_numberOfTextures = 26;
+    inline static constexpr uint8_t m_numberOfTextures = 33;
     inline const static string m_textureNames[m_numberOfTextures] = {"circle.png", "empty_circle.png", 
         "bb.png", "bw.png", "kb.png", "kw.png", "nb.png", "nw.png", "pb.png", "pw.png",
         "qb.png", "qw.png", "rb.png", "reset.png", "rw.png", "dropDown.png", "flip.png",
         "arrow_n1x.png", "arrow_n2x.png", "arrow_n3x.png", "arrow_n4x.png", "arrow_n5x.png",
-        "arrow_n6x.png", "arrow_n7x.png", "arrow_Nur.png", "arrow_Nru.png"};
+        "arrow_n6x.png", "arrow_n7x.png", "arrow_Nur.png", "arrow_Nru.png", "arrow_d1x.png",
+        "arrow_d2x.png","arrow_d3x.png","arrow_d4x.png","arrow_d5x.png","arrow_d6x.png",
+        "arrow_d7x.png"};
+
     inline const static string m_fontNames[1] = {"Arial.ttf"};
     // audioVector m_audios;
     inline static textureMap m_textures;
