9cc9c17ab667d85c7d12de967d1d6e05808bc1e7
hexaquarks <seseriusca@gmail.com>
2022-05-25 20:59:06 -0400

    Small correction to BoardReset test

--- tests/BoardReset.cpp
+++ tests/BoardReset.cpp
@@ -18,8 +18,9 @@ TEST(BoardTests, ResetTest)
     ASSERT_EQ(blackPiecesSize, 16);
 
     // Verify that pieces are brought back to their initial square.
-    shared_ptr<Piece> whitePawn = board.getBoardTile(7, 0);
-    board.setBoardTile(5, 0, whitePawn);
+    shared_ptr<Piece> whitePawn = board.getBoardTile(0, 6);
+    board.setBoardTile(0, 4, whitePawn);
+    ASSERT_EQ(board.getBoardTile(0, 4), whitePawn);
     board.reset();
-    ASSERT_EQ(board.getBoardTile(5, 0), nullptr);
+    ASSERT_EQ(board.getBoardTile(0, 4), nullptr);
 }
