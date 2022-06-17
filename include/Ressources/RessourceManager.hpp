5f10d911cea40ac8d09cf418bdc14facaa1ecd4e
Nicolas Almerge <nicolas.almerge@gmail.com>
2022-06-17 13:24:16 +0200

    Minor design changesé

--- include/Ressources/RessourceManager.hpp
+++ include/Ressources/RessourceManager.hpp
@@ -4,7 +4,6 @@
 #include <vector>
 #include <unordered_map>
 #include <memory>
-
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 
@@ -13,6 +12,7 @@ using namespace std;
 typedef unordered_map<string, shared_ptr<sf::Texture>> textureMap;
 typedef unordered_map<string, shared_ptr<sf::SoundBuffer>> audioMap;
 typedef unordered_map<string, shared_ptr<sf::Font>> fontsMap;
+
 
 class RessourceManager
 {
@@ -42,18 +42,18 @@ private:
     inline const static string fontPath = "./assets/fonts/";
 
     inline static constexpr uint8_t m_numberOfTextures = 36;
-    inline const static string m_textureNames[m_numberOfTextures] = {"circle.png", "empty_circle.png",
+    inline const static string m_textureNames[m_numberOfTextures] = {
+        "circle.png", "empty_circle.png",
         "bb.png", "bw.png", "kb.png", "kw.png", "nb.png", "nw.png", "pb.png", "pw.png",
         "qb.png", "qw.png", "rb.png", "reset.png", "resetWhite.png", "rw.png", "dropDown.png",
         "dropDownWhite.png", "flip.png", "flipWhite.png",
         "arrow_n1x.png", "arrow_n2x.png", "arrow_n3x.png", "arrow_n4x.png", "arrow_n5x.png",
         "arrow_n6x.png", "arrow_n7x.png", "arrow_Nur.png", "arrow_Nru.png", "arrow_d1x.png",
         "arrow_d2x.png","arrow_d3x.png","arrow_d4x.png","arrow_d5x.png","arrow_d6x.png",
-        "arrow_d7x.png"};
+        "arrow_d7x.png"
+    };
 
     inline const static string m_fontNames[1] = {"Arial.ttf"};
-    // audioVector m_audios;
     inline static textureMap m_textures;
     inline static fontsMap m_fonts;
-
 };
